5f3cb40563a9a3995ab3cfeb44a62f93a921dd50
malith <malith.nipun@gmail.com>
2023-05-28 15:01:05 +0530

    Huffman and Identify Minimum Connectors Completed

--- src/Huffman/Huffman.java
+++ src/Huffman/Huffman.java
@@ -13,6 +13,10 @@ import java.util.PriorityQueue;
  * @author Sajana
  */
 public class Huffman {
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
     private static class Node implements Comparable<Node> {
         char c;
